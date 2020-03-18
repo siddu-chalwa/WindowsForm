1cdce28d05499010c94f4290fdc1ca696c552d92
SIDDU CHALWA <38785739+siddu-chalwa@users.noreply.github.com>
2020-03-19 00:09:43 +0530

    WindowsForm1

---
+++ WindowsLoginForm/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsLoginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this._Name = new System.Windows.Forms.Label();
+            this.Name_T = new System.Windows.Forms.TextBox();
+            this._Age = new System.Windows.Forms.Label();
+            this.Age_T = new System.Windows.Forms.TextBox();
+            this._Submit = new System.Windows.Forms.Button();
+            this._Reset = new System.Windows.Forms.Button();
+            this.EmailID_T = new System.Windows.Forms.TextBox();
+            this._EmailID = new System.Windows.Forms.Label();
+            this.Salary_T = new System.Windows.Forms.TextBox();
+            this._Salary = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // _Name
+            // 
+            this._Name.AllowDrop = true;
+            this._Name.AutoEllipsis = true;
+            resources.ApplyResources(this._Name, "_Name");
+            this._Name.Name = "_Name";
+            // 
+            // Name_T
+            // 
+            resources.ApplyResources(this.Name_T, "Name_T");
+            this.Name_T.Name = "Name_T";
+            // 
+            // _Age
+            // 
+            this._Age.AllowDrop = true;
+            this._Age.AutoEllipsis = true;
+            resources.ApplyResources(this._Age, "_Age");
+            this._Age.Name = "_Age";
+            // 
+            // Age_T
+            // 
+            resources.ApplyResources(this.Age_T, "Age_T");
+            this.Age_T.Name = "Age_T";
+            // 
+            // _Submit
+            // 
+            resources.ApplyResources(this._Submit, "_Submit");
+            this._Submit.Name = "_Submit";
+            this._Submit.UseVisualStyleBackColor = true;
+            this._Submit.Enter += new System.EventHandler(this.Submit_Validation);
+            // 
+            // _Reset
+            // 
+            resources.ApplyResources(this._Reset, "_Reset");
+            this._Reset.Name = "_Reset";
+            this._Reset.UseVisualStyleBackColor = true;
+            this._Reset.Enter += new System.EventHandler(this.Reset_Validation);
+            // 
+            // EmailID_T
+            // 
+            resources.ApplyResources(this.EmailID_T, "EmailID_T");
+            this.EmailID_T.Name = "EmailID_T";
+            // 
+            // _EmailID
+            // 
+            this._EmailID.AllowDrop = true;
+            this._EmailID.AutoEllipsis = true;
+            resources.ApplyResources(this._EmailID, "_EmailID");
+            this._EmailID.Name = "_EmailID";
+            // 
+            // Salary_T
+            // 
+            resources.ApplyResources(this.Salary_T, "Salary_T");
+            this.Salary_T.Name = "Salary_T";
+            // 
+            // _Salary
+            // 
+            this._Salary.AllowDrop = true;
+            this._Salary.AutoEllipsis = true;
+            resources.ApplyResources(this._Salary, "_Salary");
+            this._Salary.Name = "_Salary";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.Controls.Add(this.EmailID_T);
+            this.Controls.Add(this._EmailID);
+            this.Controls.Add(this.Salary_T);
+            this.Controls.Add(this._Salary);
+            this.Controls.Add(this._Reset);
+            this.Controls.Add(this._Submit);
+            this.Controls.Add(this.Age_T);
+            this.Controls.Add(this._Age);
+            this.Controls.Add(this.Name_T);
+            this.Controls.Add(this._Name);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label _Name;
+        private System.Windows.Forms.TextBox Name_T;
+        private System.Windows.Forms.Label _Age;
+        private System.Windows.Forms.TextBox Age_T;
+        private System.Windows.Forms.Button _Submit;
+        private System.Windows.Forms.Button _Reset;
+        private System.Windows.Forms.TextBox EmailID_T;
+        private System.Windows.Forms.Label _EmailID;
+        private System.Windows.Forms.TextBox Salary_T;
+        private System.Windows.Forms.Label _Salary;
+    }
+}
+
---
+++ WindowsLoginForm/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsLoginForm
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Constrctor
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// function to clear the fields of windows form
+        /// </summary>
+        private void TextClear()
+        {
+            Name_T.Clear();
+            Age_T.Clear();
+            Salary_T.Clear();
+            EmailID_T.Clear();
+        }
+        /// <summary>
+        /// event handler of submit button which perform validation opertation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Submit_Validation(object sender, EventArgs e)
+        {
+            ///
+            //Name Validation
+            ///
+            if (Name_T.Text == "" || Name_T.Text == null)
+            {
+                MessageBox.Show("Enter Name");
+                Name_T.Focus(); 
+                return;
+            }
+            else if (!(new Regex(@"^([A-Za-z]+)(\s[A-Za-z]+)*$").Match(Name_T.Text).Success))
+            {
+                MessageBox.Show("Enter Name");
+                Name_T.Focus();
+                return;
+            }
+            else if (Name_T.Text.Length < 7)
+            {
+                MessageBox.Show("Name Should Contain Min 6 Characters");
+                Name_T.Focus();
+                return;
+            }
+            else
+            {
+                Age_T.Focus();
+            }
+            ///
+            //Age Validation
+            ///
+            int ageV;
+            if (!(int.TryParse(Age_T.Text, out ageV)))
+            {
+                MessageBox.Show("Enter Valid Age");
+                Age_T.Focus();
+                return;
+            }
+            else if (ageV < 0 || ageV > 150)
+            {
+                MessageBox.Show("Age Limit from 1 to 150");
+                Age_T.Focus();
+                return;
+            }
+            else
+            {
+                Salary_T.Focus();
+            }
+            ///
+            //Salary Validation
+            ///
+            int salaryV;
+            if (!(int.TryParse(Salary_T.Text, out salaryV)))
+            {
+                MessageBox.Show("Enter Valid Salary");
+                Salary_T.Focus();
+                return;
+            }
+            else if (salaryV < 0)
+            {
+                MessageBox.Show("Salary can't be Negative");
+                Salary_T.Focus();
+                return;
+            }
+            else if (salaryV >= 0 && salaryV < 500)
+            {
+                MessageBox.Show("Salary can't be less then 500");
+                Salary_T.Focus();
+                return;
+            }
+            else
+            {
+                EmailID_T.Focus();
+            }
+            ///
+            //EmailID Validation
+            ///
+            if (!(new Regex(@"^[_a-z0-9-]+(.[a-z0-9-]+)@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$").Match(EmailID_T.Text).Success))
+            {
+                MessageBox.Show("Enter valid EmailID");
+                EmailID_T.Focus();
+                return;
+            }
+            else
+            {
+                ///
+                // File Operation
+                ///
+                string fileName = "dataFile.txt";
+                System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(fileName, true);
+                streamWriter.WriteLine("Name: " + Name_T.Text);
+                streamWriter.WriteLine("Age: " + Age_T.Text);
+                streamWriter.WriteLine("Salary: " + Salary_T.Text);
+                streamWriter.WriteLine("Email ID: " + EmailID_T.Text);
+                streamWriter.WriteLine();
+                streamWriter.Close();
+                TextClear();
+                Name_T.Focus();
+            }
+        }
+        /// <summary>
+        /// event handler of reset button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Reset_Validation(object sender, EventArgs e)
+        {
+            TextClear();
+            Name_T.Focus();
+        }
+    }
+}
